699953cdca26371e28de0fb24b88ab832f2a4985
Talea <talea.ziesel@web.de>
2025-09-01 18:56:15 +0200

    button style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { IonicModule } from "@ionic/angular";  // 👈 wichtig
 
 import { AppComponent } from "./app.component";
 import { NgApexchartsModule } from "ng-apexcharts";
 
 @NgModule({
-  imports: [BrowserModule, NgApexchartsModule, AppComponent],
+  declarations: [],
+  imports: [
+    BrowserModule,
+    NgApexchartsModule,
+    IonicModule.forRoot({
+      mode: 'ios'   // 👈 hier erzwingst du iOS-Design global
+    }),
+    AppComponent
+  ],
   providers: [],
 })
 export class AppModule {}
